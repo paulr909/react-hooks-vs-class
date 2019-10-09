a326a6fe486e35c0121aa7b3827a09903e25bf54
Paul Rogers <paulrogers909@protonmail.com>
2019-10-09 16:19:41 +0100

    edit App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Product from "./components/Product";
 import Stock from "./components/Stock";
-import Feed from "./components/Feed";
+// import Feed from "./components/Feed";
 
 function App() {
     return (
